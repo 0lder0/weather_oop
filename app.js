4b2759bff88d13e2e8bd4f443913467c81b018c6
Lilii <lilii-ann.older@voco.ee>
2022-11-04 20:53:44 +0200

    Set city and change weather data from LS

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+// LS object
+const ls = new LS()
+//init city data
+const initCity = ls.getCity()
+
 // Weather object
-const weather = new Weather('Tallinn');
+const weather = new Weather(initCity);
 // UI object
 const ui = new UI();
 
@@ -11,9 +16,10 @@ form.addEventListener('submit', changeWeather);
 function changeWeather(event){
     const city = document.querySelector('#city-name').value;
     weather.changeCity(city);
-    getWeather()
-    document.querySelector('#city-name').value = ''
-    event.preventDefault()
+    ls.setCity(city)
+    getWeather();
+    document.querySelector('#city-name').value = '';
+    event.preventDefault();
 }
 
 // get city weather
