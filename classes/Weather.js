ca60174001973fd54b55cd588ddaa3714c9121f0
Lilii <lilii-ann.older@voco.ee>
2022-11-04 19:34:33 +0200

    Get weather data from API

---
+++ classes/Weather.js
@@ -0,0 +1,12 @@
+class Weather {
+    constructor(city) {
+        this.city = city;
+        this.key = '2fd1b33f25581bd3bc35eecc5048f3ff';
+    }
+
+    async getWeather(){
+        const responce = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.key}`);
+        const responceData = await responce.json()
+        return responceData
+    }
+}
